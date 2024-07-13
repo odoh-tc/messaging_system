7a17c49e7e4d5b6f3c254f1eb7e354489a861b3c
Tochukwu Odoh <cashearncrypto@gmail.com>
2024-07-13 06:33:07 +0100

    messaging system

--- app/main.py
+++ app/main.py
@@ -2,11 +2,11 @@ from fastapi import FastAPI, Query
 from datetime import datetime
 import logging
 from .tasks import send_email
+import os
 
 app = FastAPI()
 
 logging.basicConfig(filename='/var/log/messaging_system.log', level=logging.INFO)
-logging.info("Manual logging test")
 
 @app.get("/")
 async def root(sendmail: str = Query(None), talktome: bool = Query(False)):
@@ -14,9 +14,19 @@ async def root(sendmail: str = Query(None), talktome: bool = Query(False)):
         send_email.delay(sendmail)
         return {"message": f"Email task queued for {sendmail}"}
     
-    if talktome is not False:
+    if talktome:
         current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         logging.info(f"Current time logged: {current_time}")
         return {"message": "Current time logged"}
     
     return {"message": "Request processed, but no action was taken"}
+
+@app.get("/logs")
+async def get_logs():
+    if os.path.exists('/var/log/messaging_system.log'):
+        with open('/var/log/messaging_system.log', 'r') as log_file:
+            log_entries = log_file.readlines()
+        formatted_logs = [{"log": entry.strip()} for entry in log_entries]
+        return {"logs": formatted_logs}
+    else:
+        return {"message": "Log file not found"}
